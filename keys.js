2567e4aedb541bad2150bf143ad3914093fd09ac
Brien Hall <brienhall@Briens-MacBook-Pro.local>
2017-08-20 01:14:55 -0500

    initial

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'IuiG6ixjZ8pjdeAdXK2t0YdgU',
+  consumer_secret: 'KDFBb9lkrbNw9O9sSsVygOuC2pTJH8kspVkysU9tGeU8pF3qin',
+  access_token_key: '826161909812584453-uWuXpzIHxHhokCFeVs8HtFXn8GzzWla',
+  access_token_secret: 'lv8XgDsKNy968rHwmA0J1zf6LewPVSuydF6kO13Bq6aI0',
+}
+
