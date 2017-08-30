c69ec92371d6a2de9114fd28b5d1ec8de69207f4
Brien Hall <brienhall@Briens-MacBook-Pro.local>
2017-08-30 00:32:42 -0500

    Spotify working. Twitter working but not parsing/displaying

--- liri.js
+++ liri.js
@@ -4,8 +4,10 @@ var inquirer = require("inquirer");
 var keys = require('./keys.js')
 
 var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 
-var query = process.argv[2];
+var command = process.argv[2];
+var query = process.argv[3];
 
 //Spotify query
 var spotify = new Spotify({
@@ -13,18 +15,43 @@ var spotify = new Spotify({
 	secret: keys.spotifyKeys.secret
 });
 
-spotify.search({type: 'track', query: query, limit: 5}, function(err, data) {
-	if (err) {
-    return console.log('Error occurred: ' + err);
-  	}
-  	else {
-	var songData = data.tracks.items[0];
-	var songResult = ("Artist: " + songData.artists[0].name + "\nTrack: " + songData.name +
-    "\nAlbum: " + songData.album.name + "\nPreview: " + songData.preview_url + "\n----------");
+var twitter = new Twitter({
+	consumer_key: keys.twitterKeys.consumer_key,
+	consumer_secret: keys.twitterKeys.consumer_secret,
+	access_token_key: keys.twitterKeys.access_token_key,
+	access_token_secret: keys.twitterKeys.access_token_secret
+})
 
-	console.log(songResult);
-	}
-});
+if(command === 'spotify-this-song') {
+	spotify.search({type: 'track', query: query, limit: 5}, function(err, data) {
+		if (err) {
+	    return console.log('Error occurred: ' + err);
+	  	}
+	  	else {
+		var songData = data.tracks.items[0];
+		var songResult = ("Artist: " + songData.artists[0].name + "\nTrack: " + songData.name +
+	    "\nAlbum: " + songData.album.name + "\nPreview: " + songData.preview_url + "\n----------");
+
+		console.log(songResult);
+		}
+	});
+}
+
+if(command === 'my-tweets') {
+	twitter.get('statuses/user_timeline',function(err,tweets,response){
+		if(err) {
+			return console.log('Error occured: ' + err);
+		}
+		else {
+		for(var i=0; i<tweets.length||i<19; i++){
+			console.log(response.tweets);
+			console.log("");
+			console.log("-------------------------");
+			console.log("");
+		}
+		}
+	});
+}
 
 
 
@@ -49,6 +76,10 @@ spotify.search({type: 'track', query: query, limit: 5}, function(err, data) {
 // do what it says function {
 // 	read text from random.txt to run one of the other commands
 // }
+
+
+
+
 
 
 
