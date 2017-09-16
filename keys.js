deaa7817faa4b2577d5b9bb10fa229afa04ee9e1
Brien Hall <brienhall@Briens-MacBook-Pro.local>
2017-09-16 09:52:40 -0500

    cleanup

--- keys.js
+++ keys.js
@@ -5,18 +5,18 @@ console.log("-------------------------");
 console.log("");
 
 exports.twitterKeys = {
-  consumer_key: 'IuiG6ixjZ8pjdeAdXK2t0YdgU',
-  consumer_secret: 'KDFBb9lkrbNw9O9sSsVygOuC2pTJH8kspVkysU9tGeU8pF3qin',
-  access_token_key: '826161909812584453-uWuXpzIHxHhokCFeVs8HtFXn8GzzWla',
-  access_token_secret: 'lv8XgDsKNy968rHwmA0J1zf6LewPVSuydF6kO13Bq6aI0',
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
 }
 
 
 exports.spotifyKeys = {
-	id: '22952cc9342f4adea5286c135b8b7f55',
-	secret: '96d2badc30294f048148679bf99cad96'
+	id: '',
+	secret: ''
 }
 
 exports.omdbKeys = {
-	key: '40e9cece'
+	key: ''
 }
